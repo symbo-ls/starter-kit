ba94f6252e1f317634f15073a4c9daebda746867
nikoloza <nika.tomadze@gmail.com>
2022-06-20 08:21:22 +0400

    added update

--- src/index.js
+++ src/index.js
@@ -6,10 +6,30 @@ import DOM from 'domql'
 import App from './app'
 import { transformEmotion } from '@domql/transform-emotion'
 
+const now = window.performance.now()
+
 const app = DOM.create(App, null, null, {
   transform: {
     transformEmotion
   }
 })
 
-console.log(app.button.ref.class)
+console.warn('CREATE', app)
+
+DOM.update({
+  time: window.performance.now() - now,
+  asd: {
+    111: {
+      props: {
+        theme: 'primary'
+      }
+    }
+  }
+}, App)
+
+const updatedApp = DOM.update({
+  ye: '11',
+  time: 'qew'
+}, App)
+
+console.log(updatedApp)
