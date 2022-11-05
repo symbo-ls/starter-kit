e29c696bf3f24aa4330c5b67b15ec03768d833f9
Nika <nika.tomadze@gmail.com>
2022-11-05 08:47:59 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,78 @@
 'use strict'
 
-import { set } from '@symbo.ls/scratch'
+import { init } from 'smbls'
 
-set({
-  color: { indigo: '#118076' }
+/**
+ * Colors
+ * @tutorial https://docs.symbols.app/color
+ */
+const COLOR = {
+  transparent: 'transparent',
+  black: 'black',
+  white: 'white',
+  orange: '#ff6600'
+}
+
+/**
+ * Gradients
+ * @tutorial https://docs.symbols.app/color
+ */
+const GRADIENT = {}
+
+/**
+ * Themes
+ * @tutorial https://docs.symbols.app/theme
+ */
+const THEME = {
+  document: {
+    variants: {
+      dark: {
+        color: 'white',
+        background: 'black'
+      },
+      light: {
+        background: 'white',
+        color: 'black'
+      }
+    }
+  }
+}
+
+/**
+ * Fonts
+ * @tutorial https://docs.symbols.app/font
+ */
+const FONT = {}
+
+/**
+ * Font families
+ * @tutorial https://docs.symbols.app/font
+ */
+const FONT_FAMILY = {}
+
+/**
+ * Typography
+ * @tutorial https://docs.symbols.app/typography
+ * @tutorial https://docs.symbols.app/sequence
+ */
+const TYPOGRAPHY = {
+  base: 16,
+  ratio: 1.2
+}
+
+/**
+ * Spacing
+ * @tutorial https://docs.symbols.app/spacing
+ * @tutorial https://docs.symbols.app/sequence
+ */
+const SPACING = {}
+
+init({
+  COLOR,
+  GRADIENT,
+  THEME,
+  TYPOGRAPHY,
+  SPACING,
+  FONT,
+  FONT_FAMILY
 })
