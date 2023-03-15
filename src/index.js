a76c7c4176c0b16b575f655f338c89c6446be211
Nika <nika.tomadze@gmail.com>
2023-03-15 13:54:47 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@
 
 import config from '../symbols.json'
 import { create } from 'smbls'
-import { Sync, DevFocus, inspectOnKey } from '@symbo.ls/socket-ui'
 
 import { App } from './app'
 import designSystem from './designSystem'
 
-create({
-  extend: [Sync, DevFocus, App]
-}, {
+create(App, {
   key: config.key,
-  designSystem,
-  editor: {
-    remote: true,
-    async: true,
-    endpoint: 'api.symbols.dev',
-    serviceRoute: 'state'
-  }
-}).then(app => {
-  inspectOnKey(app)
-  window.onpopstate = e => app.context.router(app, window.location.pathname, {}, 0, false)
+  designSystem
 })
