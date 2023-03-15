3ae982bbc59f9d483bc708a47a3c8b4e51b5dfdd
Nika <nika.tomadze@gmail.com>
2023-03-15 13:57:37 +0400

    Create

---
+++ src/pages.js
@@ -0,0 +1,18 @@
+'use strict'
+
+export default {
+  '/': {
+    H1: 'Hello Symbols',
+    Link: {
+      href: '/',
+      text: 'About us'
+    }
+  },
+  '/about': {
+    H3: 'This is boilerplate',
+    Link: {
+      href: '/',
+      text: 'Go Back'
+    }
+  }
+}
