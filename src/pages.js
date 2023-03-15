dd599281028a80868e3563098b419282e49e02f5
nikoloza <nika.tomadze@gmail.com>
2023-03-15 14:02:38 +0400

    corrected about link, added yarn lock

--- src/pages.js
+++ src/pages.js
@@ -3,13 +3,15 @@
 export default {
   '/': {
     H1: 'Hello Symbols',
+    P: 'Lorem ipsum dolor sit amet',
     Link: {
-      href: '/',
+      href: '/about',
       text: 'About us'
     }
   },
   '/about': {
-    H3: 'This is boilerplate',
+    H3: 'This is Symbols starter-kit',
+    P: 'Lorem ipsum dolor sit amet',
     Link: {
       href: '/',
       text: 'Go Back'
