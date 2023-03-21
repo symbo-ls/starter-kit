9d8390e068150abab65dbd6d11793a05c2223159
Nika <nika.tomadze@gmail.com>
2023-03-21 19:01:14 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { create } from 'smbls'
 
 import { App } from './app'
 import designSystem from './designSystem'
+import * as components from './components'
 import pages from './pages'
 
 create(App, {
   key: config.key,
   designSystem,
+  components,
   pages
 })
